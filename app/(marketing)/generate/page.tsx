ba6389fc71355cf93b88ab51ef247c43e3cdab9a
Klaus <iklaus@Klass-MacBook-Pro.local>
2024-05-01 12:55:51 -0500

    Updated color scheme

--- app/(marketing)/generate/page.tsx
+++ app/(marketing)/generate/page.tsx
@@ -5,20 +5,22 @@ import { Button } from "@/components/ui/button";
 import { PlusCircle } from "lucide-react";
 import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
-import { useState, useEffect } from "react";
+import { useState, useEffect, FormEvent } from "react";
 import { useCompletion, useChat } from 'ai/react';
 
 const TrialPage = () => {
-    const [obj, setObj] = useState(null);
-
-    const { messages, input, handleInputChange, handleSubmit } = useChat();
+    const { messages, input, handleInputChange,  handleSubmit } = useChat();
+    const [obj, setObj] = useState({});
+    
+    console.log(`Obj State: ${JSON.stringify(obj)}`);
 
     useEffect(() => {
         const content = messages[1]?.content;
         if (typeof content === 'string' && isValidJson(content)) {
+            console.log(`JSON: ${content}`)
             setObj(JSON.parse(content));
         }
-    }, [messages]);
+    }, [messages, obj]);
     
     function isValidJson(json: string) {
         try {
@@ -46,26 +48,23 @@ const TrialPage = () => {
                         onChange={handleInputChange}
                     />
                     <p className="w-full text-md font-normal mt-3 text-wrap">
-                        {(obj && typeof obj === 'object') && (
-                            <div>
-                                <p style={{color: (obj as any).Hex }}>{(obj as any).HTML_Color_Name}</p>
-                                <p>{(obj as any).Hex}</p>
-                                <p>{(obj as any).RGB}</p>
-                            </div>
-                        )}
-                        {/* {messages.map(m => (
-                            <div key={m.id}>
-                                
-                            {m.content}
-                            </div>
-                        ))} */}
+                    {/* {obj && Object.entries(obj).map(([key, value]) => (
+                        <div key={key}>
+                            <p style={{color: (value as any).Hex }}>HTML Color Name: {(value as any).HTML_Color_Name}</p>
+                            <p>Hex: {(value as any).Hex}</p>
+                            <p style={{color: (obj as any).RGB }}>RGB: {(value as any).RGB}</p>
+                        </div>
+                    ))} */}
                     </p>
                 </CardContent>
                 <CardFooter>
-                <Button type="submit">
+                    <Button 
+                        type="submit"
+                    >
                         Generate  
                     </Button>
                     <Button
+                        type="button"
                         variant="outline"
                         className="ml-2 border-2 border-solid border-black hover:bg-black hover:text-white hover:transition-all hover:duration-500 hover:ease-in-out"
                     >
@@ -74,6 +73,15 @@ const TrialPage = () => {
                 </CardFooter>
                 </form>
             </Card>
+            <div className="w-[80%] border border-solid border-red-500 flex justify-start">
+                {(obj && typeof obj === 'object') && (
+                    <div className="w-[200px] h-[300px] rounded-lg p-6" style={{ backgroundColor: (obj as any).Hex}}>
+                        <p>{(obj as any).HTML_Color_Name}</p>
+                        <p>{(obj as any).Hex}</p>
+                        <p>{(obj as any).RGB}</p>
+                    </div>
+                )}
+            </div>
         </div>
     );
 }
--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -15,20 +15,57 @@ export async function POST(req: Request) {
   // Ask OpenAI for a streaming completion given the prompt
     const response = await openai.chat.completions.create({
     model: 'gpt-3.5-turbo',
-    max_tokens: 256,
+    max_tokens: 1024,
     stream: true,
+    temperature: 0.9,
+    top_p: 1,
+    response_format: { "type": "json_object" },
     messages: [
       {
         role: 'user',
-        content:  `Given the mood "${prompt}", generate a palette of four colors. Each color should be described with its HTML color name, hex code, and RGB values. Output the palette as a 4 array JSON object with each color's details in the following format: [{ "HTML_Color_Name": "<HTML Color Name>", "Hex": "<Hex values>", "RGB": "<RGB values>" }]. Only return the JSON object.NO JSON OBJECT NAME. No outside object words.`,
+        content: `i'm feeling sad today.`,
       },
+      {
+        role: 'system',
+        content: `[
+          {
+            "HTML_Color_Name": "<HTML Color Name>",
+            "Hex": "<Hex color code>",
+            "RGB": "<RGB values>"
+          },
+          {
+            "HTML_Color_Name": "<HTML Color Name>",
+            "Hex": "<Hex color code>",
+            "RGB": "<RGB values>"
+          },
+          {
+            "HTML_Color_Name": "<HTML Color Name>",
+            "Hex": "<Hex color code>",
+            "RGB": "<RGB values>"
+          },
+          {
+            "HTML_Color_Name": "<HTML Color Name>",
+            "Hex": "<Hex color code>",
+            "RGB": "<RGB values>"
+          }
+        ]`,
+      },
+      {
+        role: 'system',
+        content:  `Given the mood "${prompt}", generate four different colors. Each color should be described with its HTML color name, hex code, and RGB values. Output the palette as a ((four)) JSON objects with each color's details in the following format: 
+        {
+          "HTML_Color_Name": "", 
+          "Hex": "", 
+          "RGB": "",
+        } .Only return the JSON object.NO JSON OBJECT NAME\n
+        It MUST be a TOTAL of FOUR OBJECTS in ONE array.`,
+      }
     ],
-    response_format: { "type": "json_object" },
   });
  
   // Convert the response into a friendly text-stream
   const stream = OpenAIStream(response);
-
+  console.log(`OBJECT: ${response}`);
   // Respond with the stream
   return new StreamingTextResponse(stream);
 }
